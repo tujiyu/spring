51ae98ccda73ca5abfe70876c7630471e9db0e2a
yfdu <982140786@qq.com>
2020-03-03 16:34:03 +0800

    eureka

---
+++ spring-eureka/src/main/java/com/yufan/springeureka/SpringEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.yufan.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaApplication.class, args);
+    }
+
+}
